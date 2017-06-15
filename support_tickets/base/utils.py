0477cd8037ec8cf74f01611ab4fc1d8cf054c587
Baptiste Jonglez <git@bitsofnetworks.org>
2017-06-15 22:40:55 +0200

    templates: Fix compatibility with Django >= 1.10

--- support_tickets/base/utils.py
+++ support_tickets/base/utils.py
@@ -7,7 +7,6 @@ import uuid
 
 from django.contrib.sites.shortcuts import get_current_site
 from django.core.mail import EmailMultiAlternatives
-from django.template import Context
 from django.template.loader import get_template
 
 from ..conf import settings as app_settings
@@ -19,8 +18,8 @@ def send_email(request, subject, to, from_email, ctx, txt_template, html_templat
     ctx.update({'website_domain': get_current_site(request).domain})
 
     # define text and HTML content
-    text_content = get_template(txt_template).render(Context(ctx))
-    html_content = get_template(html_template).render(Context(ctx))
+    text_content = get_template(txt_template).render(ctx)
+    html_content = get_template(html_template).render(ctx)
 
     # Define email message objects
     msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
